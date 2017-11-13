cf3f7f695934e2b1dfa7fbe89ef71ea4bd5a1af7
FahmiAndrean <32425616+FahmiAndrean@users.noreply.github.com>
2017-11-13 09:28:59 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -2,18 +2,18 @@ const LineConnect = require('./connect');
 let LINE = require('./main.js');
 console.info("\n\
 =========================================\n\
-BotName: LINE Etot JS\n\
-Version: 0.2.1\n\
+BotName: CYBER OPERATION TEAM\n\
+Version: 0.2.2\n\
 Terima Kasih Kepada @Alfathdirk @TCR_TEAM\n\
 =========================================\n\
-\nNOTE : Ini Adalah AlphatJS Lama Buatan @Alfathdirk @TCR_TEAM Dan Ini Telah Dikembangin Oleh @TAB_TEAM\nTolong Untuk Tidak Perjual-Belikan Script Ini!\n\
-****Nekopoi.host Running****");
+\nNOTE : Ini Adalah AlphatJS Lama Buatan @Alfathdirk @TCR_TEAM Dan Ini Telah Dikembangin Oleh @TAB_TEAM Dan @Cyber Operation Team\nTolong Untuk Tidak Perjual-Belikan Script Ini!\n\
+****C.O.T RUNNING****");
 
 const auth = {
-	authToken: ' Toket Taro Sini Eh Token Maksudnya ',
-	certificate: ' Taro Cerf Disini ',
+	authToken: 'EmeGUtgc8BFNUeRF9cd9.AfdsfZNb+Y36G5zWBVvT3z8EIq.lr8i199b3cf3TBwZuWtG+soB1uVO+i+T2Z1QuAyrdsY=',
+	certificate: 'a19ae35e470502f8aa4c1c54fdfdsf81ed3b17761905836d08a1418bc08e5305c472f',
 }
-// let client =  new LineConnect(auth);
+//let client =  new LineConnect(auth);
 let client =  new LineConnect();
 
 client.startx().then(async (res) => {
